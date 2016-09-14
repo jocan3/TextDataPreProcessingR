b41ad106fb95c3a3cfbb3a6fe6a14c5078488493
Andrés Mena Arias <Andrs Mena Arias>
2016-09-13 19:44:28 -0600

    Initial commit

---
+++ WordsVector.R
@@ -0,0 +1,21 @@
+
+library(RTextTools)
+
+inputFile <- "D:/GIT/ReutersXMLToCSV/DataSamples/reut2-005.csv"
+dataSize <- 526
+trainSize <- 400
+testSize <- 126
+
+
+dataSet = read.csv(inputFile)  # read csv file 
+
+matrix <- create_matrix(dataSet$text, language="english",
+                        removeNumbers=TRUE, stemWords=TRUE, removeStopwords=TRUE, 
+                          stripWhitespace=TRUE, toLower=TRUE)
+
+container <- create_container(matrix,dataSet$class,trainSize=1:trainSize, testSize=(trainSize+1):dataSize,
+                              virgin=FALSE)
+
+write.csv(matrix, file = "test.csv")
+
+
