68d2e5f85d1f2c1984f2285b071682f83ccbfb3b
Andrés Mena Arias <Andrs Mena Arias>
2016-09-13 19:55:50 -0600

    Added weighting TF-IDF

--- WordsVector.R
+++ WordsVector.R
@@ -11,7 +11,11 @@ dataSet = read.csv(inputFile)  # read csv file
 
 matrix <- create_matrix(dataSet$text, language="english",
                         removeNumbers=TRUE, stemWords=TRUE, removeStopwords=TRUE, 
-                          stripWhitespace=TRUE, toLower=TRUE)
+                          stripWhitespace=TRUE, toLower=TRUE, weighting=tm::weightTfIdf)
+
+matrixNoTFIDF <- create_matrix(dataSet$text, language="english",
+                        removeNumbers=TRUE, stemWords=TRUE, removeStopwords=TRUE, 
+                        stripWhitespace=TRUE, toLower=TRUE)
 
 container <- create_container(matrix,dataSet$class,trainSize=1:trainSize, testSize=(trainSize+1):dataSize,
                               virgin=FALSE)
